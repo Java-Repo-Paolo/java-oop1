6ce6adad80241a346ad84e0eb3d6281635899adc
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-20 17:31:40 +0200

    Commit 1 - Java Shop

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,39 @@
+package org.lessons.java.shop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Product productOne = new Product("Passata di pomodoro",
+                                         "Una passata fatta solo con pomodori italiani",
+                                         19.00,
+                                         22);
+        Product productTwo = new Product("Passata di pomodoro",
+                                         "Una passata fatta solo con pomodori italiani",
+                                         25.00,
+                                         22);
+        Product productThree = new Product("Passata di pomodoro",
+                                           "Una passata fatta solo con pomodori italiani",
+                                           25.00,
+                                           22);
+
+        System.out.println(
+                            '\n' + "Name/Code: " + productOne.nameCode() +
+                            //'\n' + "Name: " + productOne.getName() +
+                            //'\n' + "Description: " + productOne.getDescription() +
+                            //'\n' + "Price: " + productOne.getPrice() + " €" +
+                            '\n' + "Price/Iva: " + productOne.priceIva() + " €"+
+                            '\n' +
+                            '\n' + "Name/Code: " + productTwo.nameCode() +
+                            '\n' + "Name/Code: " + productTwo.priceIva() + " €"+
+                            '\n' +
+                            '\n' + "Name/Code: " + productThree.nameCode() +
+                            '\n' + "Name/Code: " + productThree.priceIva() + " €"
+        );
+
+
+    }
+
+}
---
+++ src/org/lessons/java/shop/Product.java
@@ -0,0 +1,68 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Product {
+
+    //Attributi
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private int iva;
+
+    //Costruttori
+    public Product(String name, String description, double price, int iva){
+        Random random = new Random();
+        this.code = random.nextInt(100000);
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+    //Metodi - Getter e Setter
+
+    //Codice
+    public int getCode() {
+        return code;
+    }
+    //Nome
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //Descrizione
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    //Prezzo
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    //Iva
+    public int getIva() {
+        return iva;
+    }
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+    //Prezzo + iva
+    public double priceIva(){
+        this.price = price * (1 + (double) this.iva / 100);
+        return price;
+    }
+    public String nameCode(){
+        return name + ' ' + getCode();
+    }
+
+
+}
